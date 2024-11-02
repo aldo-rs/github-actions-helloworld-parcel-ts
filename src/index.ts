451846006eb800fedcbea4b7f02d36bf347e8ede
Aldo Rodríguez <ae.rodriguez.ext@orangebank.es>
2024-11-03 00:43:32 +0100

    Change greeting message

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,6 @@ const app = document.getElementById("app");
 // 2. Create a new <p></p> element programmatically
 const h1 = document.createElement("h1");
 // 3. Add the text content
-h1.textContent = "Hello, World!";
+h1.textContent = message;
 // 4. Append the p element to the div element
 app?.appendChild(h1);
