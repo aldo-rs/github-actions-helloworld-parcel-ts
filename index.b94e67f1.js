2c51ddb89a63ba9d40eadca8e0af99160deabb49
aldo-rs <71456248+aldo-rs@users.noreply.github.com>
2024-11-02 23:54:37 +0000

    Deploying to gh-pages from @ aldo-rs/github-actions-helloworld-parcel-ts@bc7c951db0f9b068ea118f80be00888afb896819 🚀

---
+++ index.b94e67f1.js
@@ -0,0 +1,2 @@
+var e="Hello World!! \uD83D\uDE4C";console.log(e);const t=document.getElementById("app"),n=document.createElement("h1");n.textContent=e,t?.appendChild(n);
+//# sourceMappingURL=index.b94e67f1.js.map
